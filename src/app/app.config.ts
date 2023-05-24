31fb68ce794db311e7eb1397ead0cc837c1e1129
Mircea Rila <mrila@cmedtechnology.com>
2023-05-24 20:13:34 +0300

    added withEnabledBlockingInitialNavigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
 import { provideContent, withMarkdownRenderer } from '@analogjs/content';
+import { withEnabledBlockingInitialNavigation } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideFileRouter(),
+    provideFileRouter(withEnabledBlockingInitialNavigation()),
     provideHttpClient(),
+    provideAnimations(),
     provideClientHydration(),
     provideContent(withMarkdownRenderer()),
   ],
